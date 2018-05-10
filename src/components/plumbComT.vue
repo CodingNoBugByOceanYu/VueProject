d40247294a7b8909fcdd4758f2f6caa8ea8c0e0d
BigHai <myqq1017482155@gmail.com>
2018-05-10 13:55:58 +0800

    [作业开发]工具栏样式调整

--- src/components/plumbComT.vue
+++ src/components/plumbComT.vue
@@ -13,8 +13,37 @@
         <div id="a333" class="item" style="margin-top:200px; margin-left:400px;"></div>
         <div id="a444" class="item" style="margin-top:300px; margin-left:300px;"></div>
 
+        <dl class="process-operation">
+            <dd>
+                <span class="process-cancle first" ng-click="deleteSelected()">撤销</span>
+            </dd>
+            <dd><span class="process-checkIn" >检入</span></dd>
+            <dd><span class="process-test" >仿真测试</span></dd>
+            <dd><span class="process-save" >保存设计</span></dd>
+        </dl>
+        <div class="releaseArea">
+            <a class="process-release"></a>  
+        </div>
+
         <div class="save">
-            <input type="button" value="保存" onclick="save()" />
+            <div class="operaDiv">
+                <p>编码</p>
+                <div>
+                    <input class="input" placeholder="请输入编码" />
+                </div>
+            </div>
+            <div class="operaDiv">
+                <p>名称</p>
+                <div>
+                    <input class="input" placeholder="请输入名称" />
+                </div>
+            </div>
+            <div class="operaDiv">
+                <p>描述</p>
+                <div>
+                    <textarea class="textarea"> </textarea>
+                </div>
+            </div>
         </div>
     </div>
 </template> 
@@ -73,12 +102,14 @@
 }
 
 .save {
+    margin-top: 6%;
     overflow-y: auto;
     right: 0px;
-    width: 200px;
-    height: 100%;
+    width: 240px;
+    height: 86%;
     background: #fff;
     position: fixed;
+    font-size: 14px;
 }
 
 .drag-box {
@@ -92,5 +123,74 @@
     margin-top: 20px;
     margin-left: 12px
 }
+.input:hover, .textarea:hover {
+    background-color: #fff;
+}
 
+.input, .textarea {
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    border-color: #b5b5b5;
+    border: 1px solid #34e661;
+    border-radius: 3px;
+    color: #363636;
+    display: -webkit-inline-box;
+    display: -ms-inline-flexbox;
+    display: inline-flex;
+    font-size: 12px;
+    height: 32px;
+    -webkit-box-pack: start;
+    -ms-flex-pack: start;
+    justify-content: flex-start;
+    line-height: 24px;
+    padding-left: 8px;
+    padding-right: 8px;
+    position: relative;
+    vertical-align: top;
+    box-shadow: inset 0 1px 2px hsla(0,0%,4%,.1);
+    max-width: 100%;
+    width: 180px;
+}
+
+.operaDiv:first-child {
+    margin-top: 20%;
+}
+.operaDiv {
+    margin-left: 25px;
+    margin-top: 10%;
+}
+
+.process-operation {
+    right: 70px;
+}
+
+.process-operation dd span.process-cancle {
+    background: url('../../static/img/new/形状-10-拷贝.png') no-repeat 10px center;
+}
+.process-operation dd span.process-checkIn {
+    background: url('../../static/img/new/形状-2.png') no-repeat 10px center;
+}
+.process-operation dd span.process-test {
+    background: url('../../static/img/new/测试-(1).png') no-repeat 10px center;
+}
+
+.releaseArea {
+    width: 36px;
+    background: white;
+    right: 20px;
+    position: fixed;
+    top: 115px;
+}
+.process-release {
+    display:block;
+    height: 36px;
+    width: 19px;
+    margin-left: 7px;
+    background-image:url('../../static/img/new/发布-(2).png');
+    background-repeat: no-repeat;
+    background-position:50% 50%;
+}
 </style>
